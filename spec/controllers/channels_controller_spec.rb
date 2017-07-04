79a737e517ecade647a43845295222c844f69445
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-07-04 19:47:44 +0300

    wefwe

--- spec/controllers/channels_controller_spec.rb
+++ spec/controllers/channels_controller_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe ChannelsController, type: :controller do
     get :show, params: { id: 'UCX0nHcqZWDSsAPog-LXdP7A' }, format: :atom
     expect(response).to be_success
 
-    expect(response.body).to eq '11212'
     data = Hash.from_xml response.body
+
+    puts data.inspect
+    expect(data).to have_key :wfwd
   end
 end
