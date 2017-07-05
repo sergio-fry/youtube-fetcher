bebaa4316e49da4380336ec49a4d3bc229025c8f
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-07-05 08:22:38 +0300

    mp3 downloader

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,7 +1,15 @@
 class MediaController < ApplicationController
+  def audio
+    send_data fetch_audio(params[:id]), type: 'audio/mpeg'
+  end
+
   private
 
   def fetch_audio(id)
-    `youtube-dl --extract-audio --audio-format mp3 --audio-quality 7 https://www.youtube.com/watch?v=#{id}`
+    cmd = "youtube-dl --extract-audio --audio-format mp3 --audio-quality 7 -o '#{Rails.root.join('tmp', 'youtube', '%(id)s.%(ext)s')}' https://www.youtube.com/watch?v=#{id}"
+
+    puts cmd
+    `#{cmd}`
+    Rails.root.join('tmp', 'youtube', id, '.mp3')
   end
 end
