d4657ca3e7503bf1e350141007770a2d2f4de12d
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-07-15 07:43:47 +0300

    display podcast episodes as list closes #25

---
+++ spec/views/channels/show.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "channels/show", type: :view do
+  subject { rendered }
+  before do
+    assign(:channel, double(:channel, title: 'My Podcast'))
+    assign(:podcast, FactoryGirl.build(:podcast, episodes: [FactoryGirl.build(:episode, title: 'My Episode')]))
+
+    render
+  end
+
+  it { is_expected.to include 'My Podcast' }
+  it { is_expected.to include 'My Episode' }
+end
