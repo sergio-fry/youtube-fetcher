89b72053d04e110d8cfa0783a6b81e344c0e7073
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-07-15 11:00:34 +0300

    precompie assets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,14 +7,14 @@ if [ "$1" = 'web' ]; then
   rm -rf public/uploads/
   ln -s /uploads public/uploads
 
-  bundle exec rake db:create db:migrate
+  bundle exec rake assets:precompile db:create db:migrate
   bundle exec rails server puma -p 80 --binding 0.0.0.0
 fi
 
 if [ "$1" = 'test' ]; then
   echo 'Specs...'
   export RAILS_ENV=test
-  bundle exec rake db:create db:migrate
+  bundle exec rake assets:precompile db:create db:migrate
   bundle exec rspec
 
   exit 0
