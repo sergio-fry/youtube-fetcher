cddf4853f0cb56d82ca7cc50b372de1e7eb9e9c8
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-08-03 16:00:57 +0300

    normalize audio

---
+++ spec/services/normalizer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Normalizer do
+  before do
+    `rm -rf #{Rails.root.join('tmp', 'test', 'normalizer')}`
+    `mkdir -p #{Rails.root.join('tmp', 'test', 'normalizer')}`
+  end
+
+  let(:normalizer) { Normalizer.new }
+  let(:audio_file) do
+    src = Rails.root.join('spec', 'fixtures', 'audio.mp3')
+    dst = Rails.root.join('tmp', 'test', 'normalizer', 'audio.mp3')
+    `cp #{src} #{dst}`
+    puts dst
+
+    dst
+  end
+
+
+  it 'should normalize' do
+    expect(File.exists?(normalizer.normalize(audio_file))).to eq true
+  end
+end
