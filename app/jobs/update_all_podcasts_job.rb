af0ec96ecf42dc6eedde59d8fe44809245f8adb8
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-08-07 10:05:31 +0300

    update active podcasts more often

--- app/jobs/update_all_podcasts_job.rb
+++ app/jobs/update_all_podcasts_job.rb
@@ -4,6 +4,7 @@ class UpdateAllPodcastsJob < ApplicationJob
   def perform
     Podcast.find_each do |podcast|
       next unless is_popular?(podcast)
+      next if is_fresh_enough?(podcast)
       UpdatePodcastJob.perform_later(podcast)
     end
   end
@@ -12,5 +13,21 @@ class UpdateAllPodcastsJob < ApplicationJob
 
   def is_popular?(podcast)
     podcast.accessed_at > 1.day.ago
+  end
+
+  def is_fresh_enough?(podcast)
+    new_episode_is_predicted?(podcast) && has_not_been_updated_more_than_hour?(podcast)
+  end
+
+  def new_episode_is_predicted?(podcast)
+    (podcast.updated_at > episodes_period(podcast).ago)
+  end
+
+  def has_not_been_updated_more_than_hour?(podcast)
+    (podcast.updated_at > 1.hour.ago)
+  end
+
+  def episodes_period(podcast)
+    1.hour / [podcast.episodes_per_hour, 1].max
   end
 end
--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, type: :model do
+  let(:podcast) { FactoryGirl.create :podcast }
+
+  describe '#episodes_per_week' do
+    subject { podcast.episodes_per_week }
+
+    context 'when has no episodes' do
+      it { is_expected.to eq 0 }
+    end
+
+    context do
+      before do
+        FactoryGirl.create :episode, podcast: podcast, published_at: 1.day.ago
+      end
+
+      it { is_expected.to eq 1 }
+    end
+
+    context do
+      before do
+        FactoryGirl.create :episode, podcast: podcast, published_at: 2.days.ago
+        FactoryGirl.create :episode, podcast: podcast, published_at: 1.day.ago
+      end
+
+      it { is_expected.to eq 2 }
+    end
+  end
+
+  describe '#episodes_per_hour' do
+    it 'should be (24 * 7) time less than episodes_per_week' do
+      allow(podcast).to receive(:episodes_per_week) { 24 * 7 }
+      expect(podcast.episodes_per_hour).to eq 1
+    end
+  end
 end
