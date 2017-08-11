7bf6def7a0adcdbc2968879b4732aee7e7d446bb
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-08-11 22:06:47 +0300

    stats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,28 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound do
     render plain: 'Not found', status: 404
   end
+
+  helper_method :stats
+
+  private
+
+  include ActionView::Helpers::NumberHelper
+  def stats(key)
+    case key.to_sym
+    when :jobs
+      Delayed::Job.count
+    when :podcasts
+      Podcast.count
+    when :audo_count
+      AudioEpisode.count
+    when :audio_size
+      number_to_human_size AudioEpisode.sum(:media_size)
+    when :video_count
+      AudioEpisode.count
+    when :video_size
+      number_to_human_size VideoEpisode.sum(:media_size)
+    else
+      nil
+    end
+  end
 end
