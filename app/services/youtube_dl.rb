1632252397cd05b1b5df43beb5737b46c373b4ca
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-08-27 10:21:54 +0300

    limit max size 1Gb

--- app/services/youtube_dl.rb
+++ app/services/youtube_dl.rb
@@ -2,14 +2,17 @@ class YoutubeDl
   class UnknownError < StandardError; end;
   class IncompleteYoutubeId < UnknownError; end;
 
+  MAX_FILE_SIZE = '1G'.freeze
+
+
   def fetch_audio(id)
-    exec "--extract-audio --audio-format mp3 --audio-quality 9 -o '#{Rails.root.join('tmp', 'youtube', 'audios', '%(id)s.%(ext)s')}' https://www.youtube.com/watch?v=#{id}"
+    exec "--extract-audio --audio-format mp3 --audio-quality 9 --max-filesize #{MAX_FILE_SIZE} -o '#{Rails.root.join('tmp', 'youtube', 'audios', '%(id)s.%(ext)s')}' https://www.youtube.com/watch?v=#{id}"
 
     normalizer.normalize Rails.root.join('tmp', 'youtube', 'audios', "#{id}.mp3")
   end
 
   def fetch_video(id)
-    exec "-f 'best[ext=mp4][height<=480]/worst[ext=mp4]' -o '#{Rails.root.join('tmp', 'youtube', 'videos', '%(id)s.%(ext)s')}' https://www.youtube.com/watch?v=#{id}"
+    exec "-f 'best[ext=mp4][height<=480]/worst[ext=mp4]' --max-filesize #{MAX_FILE_SIZE} -o '#{Rails.root.join('tmp', 'youtube', 'videos', '%(id)s.%(ext)s')}' https://www.youtube.com/watch?v=#{id}"
 
     Rails.root.join('tmp', 'youtube', 'videos', "#{id}.mp4")
   end
