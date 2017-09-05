a924d86eb8752b2758abd8be053f7dde993df548
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-09-05 07:41:24 +0300

    Update stats once

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,6 +1,6 @@
 begin
   Delayed::Job.where.not(cron: nil).delete_all
-  UpdateStatsJob.set(cron: '*/10 * * * *').perform_later
+  UpdateStatsJob.set(cron: '45 * * * *').perform_later
   UpdateAllPodcastsJob.set(cron: '*/10 * * * *').perform_later
   CleanupLocalMediaJob.set(cron: '3 * * * *').perform_later
   CleanupOutdatedJob.set(cron: '33 * * * *').perform_later
