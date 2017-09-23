1c1463c9feaeba43068fe350cf7cea49731dd962
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-09-23 13:12:46 +0400

    Pagination fix (#102)
    * pagination fixed
    
    * without videos view
    
    * pagination feature removed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,6 +4,6 @@ class VideosController < ApplicationController
   end
 
   def index
-    @videos = Episode.select('DISTINCT ON (origin_id) *').page(params[:page]).per(10)
+    @videos = VideosRepository.new.page(params[:page]).per(10)
   end
 end
---
+++ app/repositories/videos_repository.rb
@@ -0,0 +1,15 @@
+class VideosRepository
+  SUBQUERY = <<-SQL
+    SELECT DISTINCT ON (origin_id) * FROM episodes
+  SQL
+
+  def page(number)
+    scope = Episode.select('*').from("(#{SUBQUERY}) AS videos").page(number)
+
+    def scope.count
+      Episode.connection.select_value "SELECT COUNT(*) FROM (#{SUBQUERY}) AS videos"
+    end
+
+    scope
+  end
+end
