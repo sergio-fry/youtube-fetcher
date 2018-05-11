b49b1cf3593488440dbbca0f304010c9c8df43ad
Sergei O. Udalov <sergei.udalov@gmail.com>
2018-05-11 20:17:02 +0300

    assets fix again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.assets.enabled = false
+  config.assets.quiet = false
+  config.webpacker.check_yarn_integrity = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
