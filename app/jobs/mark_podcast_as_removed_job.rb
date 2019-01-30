c64ab1dd5278f549ae5513960d7f8cdfcc26d484
Sergei O. Udalov <sergei.udalov@gmail.com>
2019-01-30 15:56:33 +0300

    WIP: Whee.

--- app/jobs/mark_podcast_as_removed_job.rb
+++ app/jobs/mark_podcast_as_removed_job.rb
@@ -1,7 +1,9 @@
 class MarkPodcastAsRemovedJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-
+  def perform(podcast)
+    unless podcast.youtube_video_list.exists?
+      podcast.update_attribute :deleted, true
+    end
   end
 end
