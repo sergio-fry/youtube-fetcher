77bb5713d2ccebcd27bffc60bc4952fb9ca1b72f
Sergei O. Udalov <sergei.udalov@gmail.com>
2019-02-13 10:43:11 +0300

    WIP: Argh! About to give up :(

--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Search do
-  let!(:podcast_moscow) { FactoryGirl.create :podcast, title: 'Moscow' }
-  let!(:podcast_london) { FactoryGirl.create :podcast, title: 'London' }
-
-  let!(:podcast_moscow) { FactoryGirl.create :podcast, title: 'Moscow' }
-  let!(:podcast_london) { FactoryGirl.create :podcast, title: 'London' }
-
   describe 'search results' do
     subject(:results) { Search.call query }
 
     context 'when search "mos"' do
       let(:query) { 'mos' }
 
+      let!(:podcast_moscow) { FactoryGirl.create :podcast, title: 'Moscow' }
+      let!(:podcast_london) { FactoryGirl.create :podcast, title: 'London' }
+
       it { is_expected.to include podcast_moscow }
       it { is_expected.not_to include podcast_london }
+
+      let!(:episode_moscow_news) { FactoryGirl.create :episode, title: 'Moscow News' }
+      let!(:episode_peter_news) { FactoryGirl.create :episode, title: 'Peter News' }
+      it { is_expected.to include episode_moscow_news }
+      it { is_expected.not_to include episode_peter_news }
     end
   end
 end
