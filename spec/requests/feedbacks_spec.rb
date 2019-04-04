9bb3c0e20b72521115f6cd166eb874902386b44a
Sergei O. Udalov <sergei.udalov@gmail.com>
2019-04-05 00:00:49 +0300

    WIP: Moved something to somewhere... goodnight...

--- spec/requests/feedbacks_spec.rb
+++ spec/requests/feedbacks_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Feedbacks", type: :request do
   describe "GET /feedbacks" do
     it "works! (now write some real specs)" do
-      get feedbacks_path
+      FactoryGirl.create :feedback
+      get feedbacks_path, params: { format: :atom }
       expect(response).to have_http_status(200)
     end
   end
