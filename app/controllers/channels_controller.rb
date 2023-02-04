db53c6c70b713e898bdc60f7596281d3d346b341
Sergei O. Udalov <sergei.udalov@gmail.com>
2023-02-04 13:10:25 +0300

    patch_03 13:10

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,7 +3,7 @@ class ChannelsController < ApplicationController
 
   def create
     if playlist_id.present?
-      create_podcast playlist_id, 'playlist', YoutubePlaylist.new(id: playlist_id).title
+      create_podcast playlist_id, 'playlist', YoutubePlaylist.new(playlist_id).title
       redirect_to playlist_path(playlist_id)
     elsif channel_id.present?
       create_podcast channel_id, nil, YoutubeChannel.new(channel_id).title
--- app/services/youtube_playlist.rb
+++ app/services/youtube_playlist.rb
@@ -1,31 +1,23 @@
-require 'nokogiri'
-require 'net/http'
+class YoutubePlaylist
+  attr_reader :id
 
-require_relative 'youtube_video_list'
-
-class YoutubePlaylist < YoutubeVideoList
-  class PlaylistItemWrapper
-    attr_reader :id, :title, :published_at
-
-    def initialize(item)
-      @id = item.id
-      @title = item.title
-      @published_at = nil
-    end
+  def initialize(id)
+    @id = id
   end
 
   def videos
-    Nokogiri::XML(
-      resp.body
-    ).css('entry').to_a.take(10)
+    feed.videos.take(10)
+  end
+
+  def title
+    feed.title
+  end
+
+  def feed
+    @feed ||= Youtube::Feed.new("https://www.youtube.com/feeds/videos.xml?playlist_id=#{@id}")
   end
 
   def exists?
-    resp.code == '200'
-  end
-
-  def resp
-    uri = URI("https://www.youtube.com/feeds/videos.xml?playlist_id=#{origin_id}")
-    Net::HTTP.get_response(uri)
+    feed.resp.code == '200'
   end
 end
